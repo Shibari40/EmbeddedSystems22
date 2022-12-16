fb36050b82281d4dcf1a8b9e3ce832bfb951a692
Samjerry <jerry.sam@ziggo.nl>
2022-12-16 11:13:25 +0100

    added header for Timer module

---
+++ Timer.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Timer.h
+ * Author: jerry
+ *
+ * Created on December 16, 2022, 11:12 AM
+ */
+
+#ifndef TIMER_H
+#define	TIMER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TIMER_H */
+
