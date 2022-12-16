4a71c3e5119effd3a401748c03798833b92b653e
Samjerry <jerry.sam@ziggo.nl>
2022-12-16 11:12:32 +0100

    added main.

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: jerry
+ *
+ * Created on December 16, 2022, 11:12 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
