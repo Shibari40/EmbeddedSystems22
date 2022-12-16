55e0c49beea91582ba2ef45e5609af16ec25d29f
Samjerry <jerry.sam@ziggo.nl>
2022-12-16 11:07:39 +0100

    added base project files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=EmbeddedSystems22.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/EmbeddedSystems22.production.hex
