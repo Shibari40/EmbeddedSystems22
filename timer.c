71a156ae06996df605553f08c19a0c991b64d1c9
Samjerry <jerry.sam@ziggo.nl>
2022-12-16 11:40:44 +0100

    Added config settings to timer.c

--- timer.c
+++ timer.c
@@ -8,6 +8,13 @@
 
 #include <xc.h>
 
+#pragma config WPUEN    =  // 1) disable weak pull-ups. 0) weak pull-ups enabled
+#pragma config INTEDG   =  // 1) interrupt on rising. 0) interrupt on falling
+#pragma config TMR0CS   =  // 1) timer source from pin. 0) timer source from clock
+#pragma config TMR0SE   =  //
+#pragma config PSA      =  //
+#pragma config PS<2:0>  = 111
+
 void main(void) {
     return;
 }
--- timer.h
+++ timer.h
@@ -8,6 +8,17 @@
 #ifndef TIMER_H
 #define	TIMER_H
 
+typedef enum _timerrate{
+    scale2 =    000,
+    scale4 =    001,
+    scale8 =    010,
+    scale16 =   011,
+    scale32 =   100,
+    scale64 =   101,
+    scale128 =  110,
+    scale256 =  111
+} timerrate;
+
 void init_timer(void);
 
 
